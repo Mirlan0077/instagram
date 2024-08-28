529aa85dc8b1e5346e801d35720fb70c11a546dc
Mirlan0077 <mirlan.0077m@gmail.com>
2024-08-28 23:06:45 +0200

    в static создал  js папку и в ней добавил likes.js
    static/js/likes.js

---
+++ webapp/static/js/likes.js
@@ -0,0 +1,40 @@
+function addLike(postId) {
+    fetch(`/api/likes/`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': getCookie('csrftoken')
+        },
+        body: JSON.stringify({ post: postId })
+    })
+    .then(response => response.json())
+    .then(data => {
+    });
+}
+
+function removeLike(likeId) {
+    fetch(`/api/likes/${likeId}/`, {
+        method: 'DELETE',
+        headers: {
+            'X-CSRFToken': getCookie('csrftoken')
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+    });
+}
+
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        let cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            let cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
